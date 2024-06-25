ac32d23c1d415954a3ee0b3dbb9567c10dc62731
Gustavo Militão <gustavo.h.militao@gmail.com>
2024-06-25 19:22:24 -0300

    remove unecessary files

--- frontend/src/lib/i18n/i18n-service.ts
+++ frontend/src/lib/i18n/i18n-service.ts
@@ -3,24 +3,7 @@
 // SPDX-License-Identifier: MPL-2.0
 
 import i18next from 'i18next';
-import en from './locales/en.json';
-import de from './locales/de.json';
-import fr from './locales/fr.json';
-import tr from './locales/tr.json';
-import id from './locales/id.json';
-import ca from './locales/ca.json';
-import it from './locales/it.json';
-import es from './locales/es.json';
-import nb_no from './locales/nb_NO.json';
-import zh_Hant from './locales/zh_Hant.json';
-import pl from './locales/pl.json';
 import pt from './locales/pt.json';
-import uk from './locales/uk.json';
-import nl from './locales/nl.json';
-import hu from './locales/hu.json';
-import vi from './locales/vi.json';
-// import uz from './locales/uz.json'
-// import zh_Hans from './locales/zh_Hans.json';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 import type { i18n } from 'i18next';
@@ -64,24 +47,7 @@ export class I18nService {
 				lookupLocalStorage: 'language'
 			}
 		});
-		this.i18n.addResourceBundle('en', 'translation', en);
-		this.i18n.addResourceBundle('de', 'translation', de);
-		this.i18n.addResourceBundle('fr', 'translation', fr);
-		this.i18n.addResourceBundle('tr', 'translation', tr);
-		this.i18n.addResourceBundle('id', 'translation', id);
-		this.i18n.addResourceBundle('it', 'translation', it);
-		this.i18n.addResourceBundle('ca', 'translation', ca);
-		this.i18n.addResourceBundle('es', 'translation', es);
-		this.i18n.addResourceBundle('nb_NO', 'translation', nb_no);
-		this.i18n.addResourceBundle('zh_Hant', 'translation', zh_Hant);
-		this.i18n.addResourceBundle('zh_Hant', 'translation', zh_Hant);
-		this.i18n.addResourceBundle('pl', 'translation', pl);
 		this.i18n.addResourceBundle('pt', 'translation', pt);
-		this.i18n.addResourceBundle('uk', 'translation', uk);
-		this.i18n.addResourceBundle('nl', 'translation', nl);
-		this.i18n.addResourceBundle('hu', 'translation', hu);
-		this.i18n.addResourceBundle('vi', 'translation', vi);
-		// this.i18n.addResourceBundle('uz', 'translation', uz);
 	}
 
 	changeLanguage(language: string): void {
